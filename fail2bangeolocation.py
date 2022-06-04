25199aebe9ad499edd501e6e561da35a9969c2c8
rubenhortas <rubenhortas@gmail.com>
2022-06-04 22:53:47 +0200

    First commit

---
+++ fail2bangeolocation.py
@@ -0,0 +1,20 @@
+import signal
+from application.utils.python_utils import exit_signal_handler
+from application.utils.python_utils import get_interpreter_version
+from crosscutting.condition_messages import print_error
+from crosscutting.constants import REQUIRED_PYTHON_VERSION
+from presentation.utils.screen import clear_screen
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, exit_signal_handler)
+
+    clear_screen()
+
+    interpreter_version = get_interpreter_version()
+
+    if interpreter_version == REQUIRED_PYTHON_VERSION:
+        pass
+    else:
+        print_error('Requires Python {0}'.format(REQUIRED_PYTHON_VERSION))
+        exit(0)
