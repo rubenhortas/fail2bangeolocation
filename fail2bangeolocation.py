f699e8c69f888f933e5f99a6fcfa3c80e0285918
rubenhortas <rubenhortas@gmail.com>
2022-06-05 00:28:46 +0200

    Added argument parser

--- fail2bangeolocation.py
+++ fail2bangeolocation.py
@@ -1,3 +1,4 @@
+import argparse
 import signal
 from application.utils.python_utils import exit_signal_handler
 from application.utils.python_utils import get_interpreter_version
@@ -12,7 +13,9 @@ if __name__ == '__main__':
     interpreter_version = get_interpreter_version()
 
     if interpreter_version == REQUIRED_PYTHON_VERSION:
-        pass
+        parser = argparse.ArgumentParser(description="Shows geolocation stats from fail2ban log")
+        parser.add_argument('-f', '--file', metavar='fail2ban_log', nargs='?', help="fail2ban log file")
+        args = parser.parse_args()
     else:
         print_error(f"{strings.REQUIRES_PYTHON} {REQUIRED_PYTHON_VERSION}")
         exit(0)
