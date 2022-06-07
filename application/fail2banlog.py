39f6abb1a3866285e334c3a9156ce8447b437057
rubenhortas <rubenhortas@gmail.com>
2022-06-07 19:18:02 +0200

    Fixed regex

--- application/fail2banlog.py
+++ application/fail2banlog.py
@@ -6,9 +6,9 @@ from crosscutting.condition_messages import print_info, print_error
 
 def analyze(log_file):
     if os.path.exists(log_file):
-        banned_ip_regex = re.compile(r"^.* (Found|Ban) \d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$")
-        unbanned_ip_regex = re.compile(r"^.* Unban \d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$")
-        banned_ips = {}
+        banned_ip_regex = re.compile(r'^.*\s(Found|Ban)\s\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}.*$')
+        unbanned_ip_regex = re.compile(r'^.*\sUnban\s\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}.*$')
+        banned_ips = set()
 
         print_info(f"{strings.ANALYZING}: {log_file}")
 
