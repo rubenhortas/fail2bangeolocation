7f965c63a768abffa1813fa17d38605014fd3caf
rubenhortas <rubenhortas@gmail.com>
2022-06-14 19:24:37 +0200

    Updated

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
-setup(name='',
+setup(name='fail2bangeolocation',
       version='1.0',
-      description='',
+      description='Identify the geographical locations of failed attempts to log in to your system',
       author='Ruben Hortas Astariz',
       author_email='rubenhortas@gmail.com',
       url='https://github.com/rubenhortas/fail2bangeolocation',
-      install_requires=[],
+      packages=['fail2bangeolocation'],
+      install_requires=['fail2bangeolocation'],
       dependency_links=[],
-      packages=[],
       )
-
