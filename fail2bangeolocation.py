6df1fae533657fb0569b04f89b72747e361f499c
rubenhortas <rubenhortas@gmail.com>
2022-06-25 20:17:35 +0200

    Working on new arguments

--- fail2bangeolocation.py
+++ fail2bangeolocation.py
@@ -7,6 +7,10 @@ from crosscutting import strings, constants
 from crosscutting.condition_messages import print_error, print_info
 from presentation.utils.screen import clear_screen
 
+OUTPUT_OPTION = 'output'
+LOG_OPTION = 'log'
+SERVER_OPTION = 'server'
+
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, exit_signal_handler)
     clear_screen()
@@ -17,33 +21,28 @@ if __name__ == '__main__':
         parser.add_argument('-c', '--show-city', default=False, action='store_true', help=f'{strings.GROUP_BY_CITY}')
         subparsers = parser.add_subparsers(help=f'{strings.THIS_OPTIONS_ARE_MUTUALLY_EXCLUSIVE}')
 
-        parser_output = subparsers.add_parser('output', help=f'{strings.OUTPUT_OPTIONS_HELP}')
-        parser_output.add_argument('output', nargs=1, help=f'{strings.OUTPUT_OPTION_HELP}')
+        parser_output = subparsers.add_parser(OUTPUT_OPTION, help=f'{strings.OUTPUT_OPTIONS_HELP}')
+        parser_output.add_argument(OUTPUT_OPTION, default=False, action='store_true',
+                                   help=f'{strings.OUTPUT_OPTION_HELP}')
 
-        parser_log = subparsers.add_parser('log', help=f'{strings.LOG_OPTIONS_HELP}')
-        parser_log.add_argument('log', nargs=1, help=f'{strings.LOG_OPTION_HELP}')
+        parser_log = subparsers.add_parser(LOG_OPTION, help=f'{strings.LOG_OPTIONS_HELP}')
+        parser_log.add_argument(LOG_OPTION, nargs=1, help=f'{strings.LOG_OPTION_HELP}')
         parser_log.add_argument('-u', '--add-unbaned', default=False, action='store_true',
                                 help=f'{strings.UNBANNED_IPS_OPTION_HELP}')
 
-        parser_server = subparsers.add_parser('server', help=f'{strings.SERVER_OPTIONS_HELP}')
-        parser_server.add_argument('server', nargs=1, help=f'{strings.SERVER_OPTION_HELP}')
+        parser_server = subparsers.add_parser(SERVER_OPTION, help=f'{strings.SERVER_OPTIONS_HELP}')
+        parser_server.add_argument(SERVER_OPTION, nargs=1, help=f'{strings.SERVER_OPTION_HELP}')
 
         args = parser.parse_args()
 
         print_info('fail2bangeolocation')
 
-        # if args.log is None and args.service is None:
-        #     # TODO: fail2ban client banned
-        #     pass
-        # elif args.log is not None and args.service is not None:
-        #     # TODO: print argument error
-        #     pass
-        # elif args.service is not None:
-        #     # TODO
-        #     pass
-        # elif args.log is not None:
-        #     pass
-        #     # analyze(args.log[0], args.add_unbaned, args.show_city)
+        if args.__contains__(OUTPUT_OPTION):
+            print_info(f'output!')
+        elif args.__contains__(LOG_OPTION):
+            analyze(args.log[0], args.add_unbaned, args.show_city)
+        elif args.__contains__(SERVER_OPTION):
+            print_info(f'server {args.server[0]}! ')
     else:
         print_error(f'{strings.REQUIRES_PYTHON} {constants.REQUIRED_PYTHON_VERSION}')
         exit(0)
