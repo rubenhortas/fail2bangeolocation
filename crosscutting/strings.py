a231e402771207d6c0f20b6d6b161757a22737de
rubenhortas <rubenhortas@gmail.com>
2022-06-25 14:50:10 +0200

    Working on new arguments

--- crosscutting/strings.py
+++ crosscutting/strings.py
@@ -1,9 +1,15 @@
 # Argument strings
-ADD_UNBANNED_IPS = "add unbaned ips"
-DESCRIPTION = "Shows geolocation attempts from fail2ban log"
-FAIL2BAN_LOG_FILE = "fail2ban log file"
-FAIL2BAN_SERVER = "fail2ban server (e.g. sshd)"
+DESCRIPTION = "Shows geolocation of failed attempts registered by fail2ban"
 GROUP_BY_CITY = "group by city"
+LOG_OPTION_HELP = "fail2ban log file"
+LOG_OPTIONS_HELP = "log -h for help"
+LONG_DESCRIPTION = "Identify the geographical locations of failed attempts to log into your system registered by fail2ban"
+OUTPUT_OPTION_HELP = "analyze all the banned IPs registered in fail2ban"
+OUTPUT_OPTIONS_HELP = "output -h for help (default without arguments)"
+SERVER_OPTION_HELP = "fail2ban server (e.g. sshd)"
+SERVER_OPTIONS_HELP = "server -h for help"
+THIS_OPTIONS_ARE_MUTUALLY_EXCLUSIVE = "This options are mutually exclusive"
+UNBANNED_IPS_OPTION_HELP = "add unbaned ips"
 
 # Condition strings
 ERROR = "ERROR"
--- fail2bangeolocation.py
+++ fail2bangeolocation.py
@@ -14,26 +14,36 @@ if __name__ == '__main__':
 
     if interpreter_version == constants.REQUIRED_PYTHON_VERSION:
         parser = argparse.ArgumentParser(description=f'{strings.DESCRIPTION}')
-        parser.add_argument('-l', '--log', nargs=1, help=f'{strings.FAIL2BAN_LOG_FILE}')
-        parser.add_argument('-s', '--service', nargs=1, help=f'{strings.FAIL2BAN_SERVER}')
-        parser.add_argument('-u', '--add-unbaned', default=False, action='store_true',
-                            help=f'{strings.ADD_UNBANNED_IPS}')
         parser.add_argument('-c', '--show-city', default=False, action='store_true', help=f'{strings.GROUP_BY_CITY}')
+        subparsers = parser.add_subparsers(help=f'{strings.THIS_OPTIONS_ARE_MUTUALLY_EXCLUSIVE}')
+
+        parser_output = subparsers.add_parser('output', help=f'{strings.OUTPUT_OPTIONS_HELP}')
+        parser_output.add_argument('-o', '--output', nargs=1, help=f'{strings.OUTPUT_OPTION_HELP}')
+
+        parser_log = subparsers.add_parser('log', help=f'{strings.LOG_OPTIONS_HELP}')
+        parser_log.add_argument('-l', '--log', nargs=1, help=f'{strings.LOG_OPTION_HELP}')
+        parser_log.add_argument('-u', '--add-unbaned', default=False, action='store_true',
+                                help=f'{strings.UNBANNED_IPS_OPTION_HELP}')
+
+        parser_server = subparsers.add_parser('server', help=f'{strings.SERVER_OPTIONS_HELP}')
+        parser_server.add_argument('-s', '--service', nargs=1, help=f'{strings.SERVER_OPTION_HELP}')
+
         args = parser.parse_args()
 
         print_info('fail2bangeolocation')
 
-        if args.log is None and args.service is None:
-            # TODO: fail2ban client banned
-            pass
-        elif args.log is not None and args.service is not None:
-            # TODO: print argument error
-            pass
-        elif args.service is not None:
-            # TODO
-            pass
-        elif args.log is not None:
-            analyze(args.log[0], args.add_unbaned, args.show_city)
+        # if args.log is None and args.service is None:
+        #     # TODO: fail2ban client banned
+        #     pass
+        # elif args.log is not None and args.service is not None:
+        #     # TODO: print argument error
+        #     pass
+        # elif args.service is not None:
+        #     # TODO
+        #     pass
+        # elif args.log is not None:
+        #     pass
+        #     # analyze(args.log[0], args.add_unbaned, args.show_city)
     else:
         print_error(f'{strings.REQUIRES_PYTHON} {constants.REQUIRED_PYTHON_VERSION}')
         exit(0)
