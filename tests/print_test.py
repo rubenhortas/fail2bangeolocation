a458ef8beddc37c140d072ad9a853ce99f3f3ab1
rubenhortas <rubenhortas@gmail.com>
2022-06-30 22:51:53 +0200

    Refactoring

--- tests/print_test.py
+++ tests/print_test.py
@@ -6,21 +6,23 @@ from crosscutting import strings
 
 class PrintTest(unittest.TestCase):
     def setUp(self):
-        self.expected_result_sorted_by_country = {'Spain': 4, 'France': 2, 'Japan': 2, 'Portugal': 1, 'USA': 1}
-        self.expected_result_sorted_by_city = {'Spain': {'Lugo': 2, 'A Coruña': 1, strings.UNKNOWN: 1},
-                                               'France': {strings.UNKNOWN: 2}, 'Japan': {'Tokyo': 1, 'Yokohama': 1},
-                                               'Portugal': {'Lisbon': 1}, 'USA': {'New York': 1}}
+        self.attempts_sorted_by_country = {'Spain': 4, 'France': 2, 'Japan': 2, 'Portugal': 1, 'USA': 1}
+
+        self.attempts_sorted_by_country_and_city = {'Spain': {'Lugo': 2, 'A Coruña': 1, strings.UNKNOWN: 1},
+                                                    'France': {strings.UNKNOWN: 2},
+                                                    'Japan': {'Tokyo': 1, 'Yokohama': 1},
+                                                    'Portugal': {'Lisbon': 1}, 'USA': {'New York': 1}}
 
         self.ips_not_geolocated = ['1.2.3.4', '4.5.6.7', '10.11.12.13.14']
 
-    def test_print_stats_by_country(self):
+    def test_print_sorted_by_country(self):
         print('Sorted by country')
-        geolocation._print_attempts(self.expected_result_sorted_by_country, False)
+        geolocation._print_attempts(self.attempts_sorted_by_country, None)
         print()
 
-    def test_print_stats_by_city(self):
+    def test_print_sorted_by_country_and_city(self):
         print('Sorted by country and city')
-        geolocation._print_attempts(self.expected_result_sorted_by_city, True)
+        geolocation._print_attempts(self.attempts_sorted_by_country, self.attempts_sorted_by_country_and_city)
         print()
 
     def test_print_not_geolocated(self):
