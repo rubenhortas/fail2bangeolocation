a25411cd35ec6321ba2e6e5b8c3bdddbacaeccd4
rubenhortas <rubenhortas@gmail.com>
2024-08-26 21:12:10 +0200

    Improved regex

--- src/fail2bangeolocation/application/fail2ban.py
+++ src/fail2bangeolocation/application/fail2ban.py
@@ -6,16 +6,17 @@ from src.fail2bangeolocation.application.utils.system import execute_command
 _FAIL2BAN_CLIENT = 'fail2ban-client'
 _BANNED = 'banned'
 _STATUS = 'status'
+_REGEX_IP = re.compile(r'\b(?:w{1,3}\.){3}\d{1,3}\b')
 
 
-def get_banned_ips(server=None):
+def get_banned_ips(server=None) -> list:
     if server is None:
         return _get_banned_ips()
     else:
         return _get_server_banned_ips(server)
 
 
-def _get_banned_ips():
+def _get_banned_ips() -> list:
     command_stdout = execute_command(_FAIL2BAN_CLIENT, _BANNED)
 
     if command_stdout:
@@ -24,18 +25,18 @@ def _get_banned_ips():
         return []
 
 
-def _parse_banned_ips(service_banned_ips):
+def _parse_banned_ips(service_banned_ips) -> list:
     ips = []
-    sbi = ast.literal_eval(service_banned_ips.decode('UTF-8'))
+    services = ast.literal_eval(service_banned_ips.decode('UTF-8'))
 
-    for service in sbi:
+    for service in services:
         for service_name in service:
             ips.extend(service[service_name])
 
     return ips
 
 
-def _get_server_banned_ips(server):
+def _get_server_banned_ips(server) -> list:
     command_stdout = execute_command(_FAIL2BAN_CLIENT, _STATUS, server)
 
     if command_stdout:
@@ -44,12 +45,5 @@ def _get_server_banned_ips(server):
         return []
 
 
-def _parse_server_banned_ips(command_stdout):
-    ips_regex = re.compile(r'(?P<ips>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}.*)')
-
-    command_stdout_ips = ips_regex.findall(command_stdout.decode('UTF-8'))
-
-    if command_stdout_ips:
-        return str.split(command_stdout_ips[0])
-    else:
-        return []
+def _parse_server_banned_ips(command_stdout) -> list:
+    return _REGEX_IP.findall(command_stdout.decode('UTF-8'))
