8ef50e881d678d5341ed7d727af4dfe611c06c3d
rubenhortas <rubenhortas@gmail.com>
2024-08-26 20:41:02 +0200

    Fixed types

--- src/fail2bangeolocation/application/handlers/exception_handler.py
+++ src/fail2bangeolocation/application/handlers/exception_handler.py
@@ -1,6 +1,6 @@
 from src.fail2bangeolocation.crosscutting.condition_messages import print_exception
 
 
-def handle_exception(exception: str) -> None:
+def handle_exception(exception: Exception) -> None:
     print_exception(exception)
     exit(1)
