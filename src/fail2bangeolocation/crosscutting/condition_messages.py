bc2220ce9a8e6af51e488212abe7d9256a3fe94a
rubenhortas <rubenhortas@gmail.com>
2024-08-26 20:27:41 +0200

    Added type hints

--- src/fail2bangeolocation/crosscutting/condition_messages.py
+++ src/fail2bangeolocation/crosscutting/condition_messages.py
@@ -1,13 +1,13 @@
 from src.fail2bangeolocation.presentation.tag import Tag
 
 
-def print_error(msg):
+def print_error(msg: str):
     print(f"{Tag.error} {msg}")
 
 
-def print_exception(msg):
+def print_exception(msg: str):
     print(f"{Tag.exception} {msg}")
 
 
-def print_info(msg):
+def print_info(msg: str):
     print(f"{Tag.info} {msg}")
