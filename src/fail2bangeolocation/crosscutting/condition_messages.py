1df03d6da739a8e650fb48caef47d634123ae82f
rubenhortas <rubenhortas@gmail.com>
2024-08-26 20:29:06 +0200

    Renamed variables

--- src/fail2bangeolocation/crosscutting/condition_messages.py
+++ src/fail2bangeolocation/crosscutting/condition_messages.py
@@ -1,13 +1,13 @@
 from src.fail2bangeolocation.presentation.tag import Tag
 
 
-def print_error(msg: str):
-    print(f"{Tag.error} {msg}")
+def print_error(error: str):
+    print(f"{Tag.error} {error}")
 
 
-def print_exception(msg: str):
-    print(f"{Tag.exception} {msg}")
+def print_exception(exception: str):
+    print(f"{Tag.exception} {exception}")
 
 
-def print_info(msg: str):
-    print(f"{Tag.info} {msg}")
+def print_info(info: str):
+    print(f"{Tag.info} {info}")
