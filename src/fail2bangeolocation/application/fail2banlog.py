a1642588e68108442629a041e4b9cfe26ec4a141
rubenhortas <rubenhortas@gmail.com>
2024-08-27 21:20:59 +0200

    Refactored

--- src/fail2bangeolocation/application/fail2banlog.py
+++ src/fail2bangeolocation/application/fail2banlog.py
@@ -1,46 +1,38 @@
-import os
 import re
 
-from src.fail2bangeolocation.application.handlers.error_handler import handle_error
 from src.fail2bangeolocation.application.handlers.exception_handler import handle_exception
 from src.fail2bangeolocation.crosscutting import strings
 from src.fail2bangeolocation.crosscutting.condition_messages import print_info
 
+_REGEX_BANNED_IPS = re.compile(r'^.*\s(Found|Ban)\s(?P<ip>(\d{1,3}.){3}\d{1,3}).*$')
+_REGEX_UNBANNED_IPS = re.compile(r'^.*\sUnban\s(?P<ip>(\d{1,3}.){3}\d{1,3}).*$')
+
 
 def get_banned_ips(log_file, add_unbanned):
-    try:
-        if os.path.exists(log_file):
-            if os.path.isfile(log_file):
-                print_info(f"{strings.ANALYZING}: {log_file}")
-
-                return _get_banned_ips(log_file, add_unbanned)
-            else:
-                handle_error(f"{log_file} {strings.IS_NOT_A_FILE}", True)
-        else:
-            handle_error(f"{log_file} {strings.DOES_NOT_EXISTS}", True)
-    except Exception as e:
-        handle_exception(e)
-
-
-def _get_banned_ips(log_file, add_unbanned):
-    banned_ip_regex = re.compile(r'^.*\s(Found|Ban)\s(?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).*$')
-    unbanned_ip_regex = re.compile(r'^.*\sUnban\s(?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).*$')
     banned_ips = set()
-    log = open(log_file, 'r')
-    lines = log.readlines()
 
-    for line in lines:
-        match = banned_ip_regex.search(line)
+    try:
+        with open(log_file, mode='r') as log:
+            print_info(f"{strings.ANALYZING}: {log_file}")
 
-        if match:
-            banned_ips.add(match.group('ip'))
-        else:
-            match = unbanned_ip_regex.search(line)
+            for line in log:
+                match = _REGEX_BANNED_IPS.search(line)
 
-            if match:
-                if add_unbanned:
+                if match:
                     banned_ips.add(match.group('ip'))
                 else:
-                    banned_ips.discard(match.group('ip'))
+                    match = _REGEX_UNBANNED_IPS.search(line)
+
+                    if match:
+                        if add_unbanned:
+                            banned_ips.add(match.group('ip'))
+                        else:
+                            banned_ips.discard(match.group('ip'))
+    except FileNotFoundError as file_not_found_error:
+        handle_exception(file_not_found_error)
+    except PermissionError as permission_error:
+        handle_exception(permission_error)
+    except OSError as os_error:
+        handle_exception(os_error)
 
     return list(banned_ips)
