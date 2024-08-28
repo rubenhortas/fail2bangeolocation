f6d9ca3c530a27987b14a2a9df3bfa00a067015b
rubenhortas <rubenhortas@gmail.com>
2024-08-28 21:24:54 +0200

    Refactored

--- tests/reallyfreegeoip_test.py
+++ tests/reallyfreegeoip_test.py
@@ -7,13 +7,12 @@ from fail2bangeolocation.application.reallyfreegeoip import get_location
 class Reallyfreegeoip(unittest.TestCase):
     def setUp(self):
         self.ip = '207.188.187.163'
-        self.result = {"ip": "207.188.187.163", "country_code": "US", "country_name": "United States",
-                       "region_code": "", "region_name": "", "city": "", "zip_code": "", "time_zone": "America/Chicago",
-                       "latitude": 37.751, "longitude": -97.822, "metro_code": 0}
+        self.expected_country = 'United States'
+        self.expected_city = ''
 
     def test_get_location(self):
         country, city = get_location(self.ip)
-        self.assertTupleEqual(('United States', ''), (country, city))
+        self.assertTupleEqual((self.expected_country, self.expected_city), (country, city))
 
 
 if __name__ == '__main__':
