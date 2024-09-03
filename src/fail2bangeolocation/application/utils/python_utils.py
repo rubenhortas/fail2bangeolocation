f7950af2d98e0d1fed2c9390cbdf27b198090e34
rubenhortas <rubenhortas@gmail.com>
2024-09-03 20:49:25 +0200

    Renamed function

--- src/fail2bangeolocation/application/utils/python_utils.py
+++ src/fail2bangeolocation/application/utils/python_utils.py
@@ -1,7 +1,7 @@
 from sys import version_info
 
 
-def get_interpreter_version() -> str:
+def get_python_interpreter_version() -> str:
     major, minor, micro, release_level, serial = version_info
 
     return major
--- src/fail2bangeolocation/fail2bangeolocation.py
+++ src/fail2bangeolocation/fail2bangeolocation.py
@@ -5,7 +5,7 @@ import signal
 
 from src.fail2bangeolocation.application.handlers.signal_handler import handle_sigint
 from src.fail2bangeolocation.application.geolocation import geolocate
-from src.fail2bangeolocation.application.utils.python_utils import get_interpreter_version
+from src.fail2bangeolocation.application.utils.python_utils import get_python_interpreter_version
 from src.fail2bangeolocation.crosscutting import constants, strings
 from src.fail2bangeolocation.crosscutting.condition_messages import print_error
 from src.fail2bangeolocation.presentation.utils.screen import clear_screen
@@ -19,7 +19,7 @@ def main():
     signal.signal(signal.SIGINT, handle_sigint)
     clear_screen()
 
-    if get_interpreter_version() == constants.REQUIRED_PYTHON_VERSION:
+    if get_python_interpreter_version() == constants.REQUIRED_PYTHON_VERSION:
         parser = argparse.ArgumentParser(description=f'{strings.DESCRIPTION}')
         parser.add_argument('-c', '--show-city', default=False, action='store_true', help=f'{strings.GROUP_BY_CITY}')
         subparsers = parser.add_subparsers(help=f'{strings.THESE_OPTIONS_ARE_MUTUALLY_EXCLUSIVE}')
