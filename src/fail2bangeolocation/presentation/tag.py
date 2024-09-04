7c312b7e538228d3433f3631ac841199d40ab287
rubenhortas <rubenhortas@gmail.com>
2024-09-04 18:52:29 +0200

    Fixed quotes

--- src/fail2bangeolocation/presentation/tag.py
+++ src/fail2bangeolocation/presentation/tag.py
@@ -3,6 +3,6 @@ from colorama import Fore, Style
 
 
 class Tag:
-    error = f'[{Style.BRIGHT}{Fore.RED}{strings.ERROR}{Style.RESET_ALL}]'
-    exception = f'[{Style.BRIGHT}{Fore.RED}{strings.EXCEPTION}{Style.RESET_ALL}]'
-    info = f'[{Style.BRIGHT}{Fore.GREEN}*{Style.RESET_ALL}]'
+    error = f"[{Style.BRIGHT}{Fore.RED}{strings.ERROR}{Style.RESET_ALL}]"
+    exception = f"[{Style.BRIGHT}{Fore.RED}{strings.EXCEPTION}{Style.RESET_ALL}]"
+    info = f"[{Style.BRIGHT}{Fore.GREEN}*{Style.RESET_ALL}]"
