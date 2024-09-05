3cb94d4c7b5a4eedab3cd85ca6debdb32f5c0571
rubenhortas <rubenhortas@gmail.com>
2024-09-05 20:00:17 +0200

    Changed comment

--- src/fail2bangeolocation/application/reallyfreegeoip.py
+++ src/fail2bangeolocation/application/reallyfreegeoip.py
@@ -7,22 +7,20 @@ REALLYFREEGEOIP_URL = 'https://reallyfreegeoip.org'
 
 
 def get_location(ip: str) -> (str, str):
-    """
-    Response:
-    '{
-      "ip":"207.188.187.163",
-      "country_code":"US",
-      "country_name":"United States",
-      "region_code":"",
-      "region_name":"",
-      "city":"",
-      "zip_code":"",
-      "time_zone":"America/Chicago",
-      "latitude":37.751,
-      "longitude":-97.822,
-      "metro_code":0
-     }'
-    """
+    # Response:
+    # '{
+    #   "ip":"207.188.187.163",
+    #   "country_code":"US",
+    #   "country_name":"United States",
+    #   "region_code":"",
+    #   "region_name":"",
+    #   "city":"",
+    #   "zip_code":"",
+    #   "time_zone":"America/Chicago",
+    #   "latitude":37.751,
+    #   "longitude":-97.822,
+    #   "metro_code":0
+    #  }'
     try:
         request_url = f'{REALLYFREEGEOIP_URL}/json/{ip}'
         response = requests.get(request_url)
