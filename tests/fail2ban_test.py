235dee3863067653eb8ba56205fe3649914b229e
rubenhortas <rubenhortas@gmail.com>
2024-09-05 20:53:17 +0200

    Formatted

--- tests/fail2ban_test.py
+++ tests/fail2ban_test.py
@@ -9,7 +9,6 @@ class Fail2banTest(unittest.TestCase):
     def setUp(self):
         self.fail2ban_banned = b"[{'sshd': ['1.1.1.1', '1.1.1.2', '1.1.1.3']}, {'http': ['2.2.2.1', '2.2.2.2', '2.2.2.3']}]\n"
         self.fail2ban_banned_expected_result = ['1.1.1.1', '1.1.1.2', '1.1.1.3', '2.2.2.1', '2.2.2.2', '2.2.2.3']
-
         self.fail2ban_status_sshd = b'''Status for the jail: sshd
                                         |- Filter
                                         |  |- Currently failed: 0
