3eff3e0a1eceb7d854f73e9e3713a35bb6ddb07e
rubenhortas <rubenhortas@gmail.com>
2024-09-07 14:30:46 +0200

    Tests refactored

--- tests/reallyfreegeoip_test.py
+++ tests/reallyfreegeoip_test.py
@@ -5,14 +5,13 @@ from src.fail2bangeolocation.application.reallyfreegeoip import get_location
 
 # noinspection SpellCheckingInspection
 class Reallyfreegeoip(unittest.TestCase):
-    def setUp(self):
-        self.ip = '207.188.187.163'
-        self.expected_country = 'United States'
-        self.expected_city = ''
-
     def test_get_location(self):
-        country, city = get_location(self.ip)
-        self.assertTupleEqual((self.expected_country, self.expected_city), (country, city))
+        ip = '207.188.187.163'
+        expected_country = 'United States'
+        expected_city = ''
+
+        country, city = get_location(ip)
+        self.assertTupleEqual((expected_country, expected_city), (country, city))
 
 
 if __name__ == '__main__':
